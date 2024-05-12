cb2c2ac865c98877c416acf84a145076c4881bcc
robjwood <treborwood@gmail.com>
2024-05-12 22:39:32 +0100

    Removed fluff

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,11 @@
-const { DateTime } = require("luxon");
-
 module.exports = function (eleventyConfig) {
 
   // Copy the following directories to the output folder 
   eleventyConfig.addPassthroughCopy("src/components");
-  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/js");
   
   // Filters
-  eleventyConfig.addFilter("postDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toISODate();
-  });
-  eleventyConfig.addFilter("postDatePretty", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
-  });
-  
   return {
     dir: {
       input: "src",
